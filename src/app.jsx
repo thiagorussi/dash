e5bc8eefa509dbcbe481823fd90f752659110957
thiagorussi <thiagorussii@gmail.com>
2024-04-23 12:59:37 -0300

    instando vite plugin

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable perfectionist/sort-imports */
-import 'src/global.css';
+import '/src/global.css';
 
 import { useScrollToTop } from 'src/hooks/use-scroll-to-top';
 
-import Router from 'src/routes/sections';
-import ThemeProvider from 'src/theme';
+import Router from '/src/routes/sections';
+import ThemeProvider from '/src/theme';
 
 // ----------------------------------------------------------------------
 
